7d6b2679ad4210adfd39c61b6e63a8801b5e1472
恩泽小贼 <moslandwez@outlook.com>
2020-09-26 09:59:35 -0500

    Enze Wang 0926
    Enze Wang 0926

--- Module2.R
+++ Module2.R
@@ -1,9 +1,9 @@
 #This is main code for Module 2
 #Please run the code line by line
 
-#Main code by CHENYANG JIANG and ENZE WANG
+#Main code edited by ENZE WANG
 
-#Fix by HanGyu KANG and RUI HUANG
+#Fix by CHENYANG JIANG, HanGyu KANG and RUI HUANG
 
 #[TOC]
 
@@ -16,7 +16,7 @@
 
 
 
-#It require the following package to plot
+#It require the following package 
 if (!require(ggplot2)){
   install.packages("ggplot2")
 }
@@ -25,6 +25,9 @@ if (!require(grid)){
 }
 if (!require(gridExtra)){
   install.packages("gridExtra")
+}
+if (!require(car)){
+  install.packages("car")
 }
 
 #Please put the Module2.R into the main Folder and run it instead of in Code Folder, or it may not find the BodyFat.csv
@@ -201,3 +204,229 @@ dim(CleanData)
 write.csv(CleanData,'Data/CleanData.csv',row.names = FALSE)
 
 
+
+#2. Model Build
+
+
+
+#Input our CleanData
+CleanData = read.csv("Data/CleanData.csv",sep=",")
+CleanData=CleanData[,-c(1,3)]
+n=dim(CleanData)[1]
+#We first test the vif of the simple linear regression
+SLR = lm(BODYFAT~.,data=CleanData) 
+vif = car::vif(SLR) 
+mean(vif) 
+vif
+
+#You can see that there is serious multicollinearity, therefore after we build our final model, we will check the multicollinearity, and our model should be as simple as possible
+jpeg(file="Image/Simple_Linear_Regression_Analysis.jpeg")
+par(mfrow=c(2,3)) 
+plot(SLR) 
+boxplot(SLR$residuals)
+dev.off()
+
+#The normality also not hold perfectly, therefore we will also care about the normality in the future model. 
+
+#We will selct the best model with log and square transformation. 
+LogAGE=log(CleanData$AGE)
+LogWEIGHT=log(CleanData$WEIGHT)
+LogHEIGHT=log(CleanData$HEIGHT)
+LogADIPOSITY=log(CleanData$ADIPOSITY)
+LogNECK=log(CleanData$NECK)
+LogCHEST=log(CleanData$CHEST)
+LogABDOMEN=log(CleanData$ABDOMEN)
+LogHIP=log(CleanData$HIP)
+LogTHIGH=log(CleanData$THIGH)
+LogKNEE=log(CleanData$KNEE)
+LogANKLE=log(CleanData$ANKLE)
+LogBICEPS=log(CleanData$BICEPS)
+LogFOREARM=log(CleanData$FOREARM)
+LogWRIST=log(CleanData$WRIST)
+
+sqAGE=CleanData$AGE^2
+sqWEIGHT=CleanData$WEIGHT^2
+sqHEIGHT=CleanData$HEIGHT^2
+sqADIPOSITY=CleanData$ADIPOSITY^2
+sqNECK=CleanData$NECK^2
+sqCHEST=CleanData$CHEST^2
+sqABDOMEN=CleanData$ABDOMEN^2
+sqHIP=CleanData$HIP^2
+sqTHIGH=CleanData$THIGH^2
+sqKNEE=CleanData$KNEE^2
+sqANKLE=CleanData$ANKLE^2
+sqBICEPS=CleanData$BICEPS^2
+sqFOREARM=CleanData$FOREARM^2
+sqWRIST=CleanData$WRIST^2
+
+ExtendData=cbind(CleanData,LogAGE,LogWEIGHT,LogHEIGHT,LogADIPOSITY,LogNECK,LogCHEST,LogABDOMEN,LogHIP,LogTHIGH,LogKNEE,LogANKLE,LogBICEPS,LogFOREARM,LogWRIST
+                 ,sqAGE,sqWEIGHT,sqHEIGHT,sqADIPOSITY,sqNECK,sqCHEST,sqABDOMEN,sqHIP,sqTHIGH,sqKNEE,sqANKLE,sqBICEPS,sqFOREARM,sqWRIST)
+
+SLR0=lm(BODYFAT~1,data=ExtendData)
+SLR_Full=lm(BODYFAT~AGE+WEIGHT+HEIGHT+ADIPOSITY+NECK+CHEST+ABDOMEN+HIP+THIGH+KNEE+ANKLE+BICEPS+FOREARM+WRIST,data=ExtendData)
+SLR_Log_Full=lm(BODYFAT~LogAGE+LogWEIGHT+LogHEIGHT+LogADIPOSITY+LogNECK+LogCHEST+LogABDOMEN+LogHIP+LogTHIGH+LogKNEE+LogANKLE+LogBICEPS+LogFOREARM+LogWRIST,data=ExtendData)
+SLR_sq_Full=lm(BODYFAT~sqAGE+sqWEIGHT+sqHEIGHT+sqADIPOSITY+sqNECK+sqCHEST+sqABDOMEN+sqHIP+sqTHIGH+sqKNEE+sqANKLE+sqBICEPS+sqFOREARM+sqWRIST,data=ExtendData)
+SLR_All_Full=lm(BODYFAT~.,data=ExtendData)
+
+# forward AIC
+step(SLR0,scope=list(upper=SLR_Full),direction='forward', k=2)
+#AIC=682.97
+#BODYFAT ~ ABDOMEN + WEIGHT + WRIST + ADIPOSITY + NECK + AGE + FOREARM + CHEST
+
+# forward AIC
+step(SLR0,scope=list(upper=SLR_Log_Full),direction='forward', k=2)
+#AIC=679.57
+#BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT + LogNECK + LogAGE + LogFOREARM
+
+# forward AIC
+step(SLR0,scope=list(upper=SLR_sq_Full),direction='forward', k=2)
+#AIC=690.23
+#BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST + sqBICEPS
+
+# forward AIC
+step(SLR0,scope=list(upper=SLR_All_Full),direction='forward', k=2)
+#AIC=676.74
+#BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST + LogADIPOSITY + LogNECK + LogBICEPS + sqBICEPS + AGE + CHEST + sqNECK + sqFOREARM
+
+# forward BIC
+step(SLR0,scope=list(upper=SLR_Full),direction='forward', k=log(n))
+#BIC=700.41
+#BODYFAT ~ ABDOMEN + WEIGHT + WRIST
+
+# forward BIC
+step(SLR0,scope=list(upper=SLR_Log_Full),direction='forward', k=log(n))
+#BIC=697.82
+#BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT
+
+# forward BIC
+step(SLR0,scope=list(upper=SLR_sq_Full),direction='forward', k=log(n))
+#BIC=705.87
+#BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST
+
+# forward BIC
+step(SLR0,scope=list(upper=SLR_All_Full),direction='forward', k=log(n))
+#AIC=697.68
+#BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST
+
+
+
+#The best model are the following, we check their VIF:
+
+
+#BODYFAT ~ ABDOMEN + WEIGHT + WRIST + ADIPOSITY + NECK + AGE + FOREARM + CHEST
+Model1 = lm(BODYFAT ~ ABDOMEN + WEIGHT + WRIST + ADIPOSITY + NECK + AGE + FOREARM + CHEST,data=ExtendData) 
+vif = car::vif(Model1) 
+mean(vif) 
+#6.085865
+max(vif)
+# 11.50582
+#Exist serious multicollinearity
+
+#BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT + LogNECK + LogAGE + LogFOREARM
+
+Model2 = lm(BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT + LogNECK + LogAGE + LogFOREARM,data=ExtendData) 
+vif = car::vif(Model2) 
+mean(vif) 
+#2.096706
+max(vif)
+# 3.163965
+#Not serious multicollinearity
+
+#BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST + sqBICEPS
+
+Model3 = lm(BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST + sqBICEPS,data=ExtendData) 
+vif = car::vif(Model3) 
+mean(vif) 
+#3.832809
+max(vif)
+# 6.772439
+#Not serious multicollinearity
+
+#BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST + LogADIPOSITY + LogNECK + LogBICEPS + sqBICEPS + AGE + CHEST + sqNECK + sqFOREARM
+
+Model4 = lm(BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST + LogADIPOSITY + LogNECK + LogBICEPS + sqBICEPS + AGE + CHEST + sqNECK + sqFOREARM,data=ExtendData) 
+vif = car::vif(Model4) 
+mean(vif) 
+#61.19585
+max(vif)
+# 212.3222
+#Exist serious multicollinearity
+
+#BODYFAT ~ ABDOMEN + WEIGHT + WRIST
+
+Model5 = lm(BODYFAT ~ ABDOMEN + WEIGHT + WRIST,data=ExtendData) 
+vif = car::vif(Model5) 
+mean(vif) 
+#3.819638
+max(vif)
+# 5.385934
+#Not serious multicollinearity
+
+#BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT
+
+Model6 = lm(BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT,data=ExtendData) 
+vif = car::vif(Model6) 
+mean(vif) 
+#1.48797
+max(vif)
+# 1.735822
+#Not serious multicollinearity
+
+#BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST
+
+Model7 = lm(BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST,data=ExtendData) 
+vif = car::vif(Model7) 
+mean(vif) 
+#3.673506
+max(vif)
+#5.134862
+#Not serious multicollinearity
+
+#BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST
+
+Model8 = lm(BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST,data=ExtendData) 
+vif = car::vif(Model8) 
+mean(vif) 
+#3.595182
+max(vif)
+# 4.976449
+#Not serious multicollinearity
+
+#In the model before, ABDOMEN is the most important, we will test is there any necessary to add other variables:
+
+model1=lm(BODYFAT ~ ABDOMEN + WEIGHT + WRIST,data=ExtendData)
+summary(model1)
+#Multiple R-squared:  0.7168,	Adjusted R-squared:  0.7133 
+model1a=lm(BODYFAT ~ ABDOMEN + WEIGHT,data=ExtendData)
+summary(model1a)
+#Multiple R-squared:  0.7053,	Adjusted R-squared:  0.7029
+model1b=lm(BODYFAT ~ ABDOMEN + WRIST,data=ExtendData)
+summary(model1b)
+#Multiple R-squared:  0.7003,	Adjusted R-squared:  0.6979 
+model2=lm(BODYFAT ~ LogABDOMEN + LogWRIST + LogHEIGHT,data=ExtendData)
+summary(model2)
+#Multiple R-squared:  0.7198,	Adjusted R-squared:  0.7163 
+model2a=lm(BODYFAT ~ LogABDOMEN + LogWRIST,data=ExtendData)
+summary(model2a)
+#Multiple R-squared:  0.7032,	Adjusted R-squared:  0.7007 
+model2b=lm(BODYFAT ~ LogABDOMEN + LogHEIGHT,data=ExtendData)
+summary(model2b)
+#Multiple R-squared:  0.6986,	Adjusted R-squared:  0.6962 
+model3=lm(BODYFAT ~ sqABDOMEN + sqWEIGHT + sqWRIST,data=ExtendData)
+summary(model3)
+#Multiple R-squared:  0.7105,	Adjusted R-squared:  0.7069 
+model3a=lm(BODYFAT ~ sqABDOMEN + sqWEIGHT,data=ExtendData)
+summary(model3a)
+#Multiple R-squared:  0.7017,	Adjusted R-squared:  0.6993 
+model3b=lm(BODYFAT ~ sqABDOMEN + sqWRIST,data=ExtendData)
+summary(model3b)
+#Multiple R-squared:  0.6903,	Adjusted R-squared:  0.6878 
+model4=lm(BODYFAT ~ ABDOMEN + sqWEIGHT + LogWRIST,data=ExtendData)
+summary(model4)
+#Multiple R-squared:  0.7199,	Adjusted R-squared:  0.7165 
+model4a=lm(BODYFAT ~ ABDOMEN + sqWEIGHT,data=ExtendData)
+summary(model4a)
+#Multiple R-squared:  0.7079,	Adjusted R-squared:  0.7056 
+model4b=lm(BODYFAT ~ ABDOMEN + LogWRIST,data=ExtendData)
+summary(model4b)
+#Multiple R-squared:  0.7003,	Adjusted R-squared:  0.6979 
