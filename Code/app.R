23b4a8061446ad5f2e82532654a1afe7ff0ba84b
恩泽小贼 <moslandwez@outlook.com>
2020-10-19 12:39:01 -0500

    Enze Wang 1019
    Enze Wang 1019

--- Code/app.R
+++ Code/app.R
@@ -4,11 +4,11 @@ library(ggplot2)
 
 #This is code for shiny app
 
-#This application is main edited by RUI HUANG
+#This application is main edited by RUI HUANG and HanGyu KANG
 
 #The interface, ggplot and unit part are fixed, edited and maintained by ENZE WANG
 
-#Other problems are fixed by HanGyu KANG and CHENYANG JIANG
+#Other problems are fixed by CHENYANG JIANG
 
 ui<-navbarPage('Bodyfat Calculator',inverse = T,collapsible = T,
                tabPanel("Group 2",
@@ -21,11 +21,11 @@ ui<-navbarPage('Bodyfat Calculator',inverse = T,collapsible = T,
                                         selectInput("Weightunit", "Weight Units:",list("lbs" = "lbs","kg" = "kg")),
                                         helpText("The value  must between 50 to 550 lbs (22.6796 to 249.476 kg)"),
                                         selectInput("Gender", "Gender:",list("Men" = "Men","Women" = "Women")),
-                                        helpText("The gender is not necessary for our model prediction, but for bodyfat suggestion"),
+                                        helpText("The gender is not necessary for our model prediction, but for body fat suggestion"),
                                         actionButton("calculate",label = "Submit",icon=icon('angle-double-right'))
                                       ),
                                       mainPanel(tabsetPanel(
-                                        tabPanel("What's Your Bodyfat",h1("Your body fat is:"),textOutput("results"),
+                                        tabPanel("What's Your Bodyfat",h1("Your body fat percentage is:"),textOutput("results"),
                                         tags$style("#results {font-size:36px;}"),
                                         textOutput("warning"),
                                         plotOutput(outputId="PiePlot"),h1("Suggestion:"),textOutput("suggestion"),style = "font-size:120%"),
@@ -86,7 +86,7 @@ server<-shinyServer(function(input, output) {
     if(outcome()=="Input Error 1"){
       paste("Be careful our application does not allow extreme input values. It seems that your input is out of normal range. Please check your input")
     }else if(outcome()=="Input Error 2"){
-      paste("Be careful our application does not allow bodyfat to be smaller than 0% and larger than 60%. It seems that your output is out of normal range. Please check your input")
+      paste("Be careful our application does not allow body fat percentage to be smaller than 0% and larger than 60%. It seems that your output is out of normal range. Please check your input")
     }else if(outcome()=="Please input your data"){
       paste("Please input your data")
     }else{}
@@ -97,16 +97,16 @@ server<-shinyServer(function(input, output) {
   output$suggestion <- renderText({
     if(outcome()!="Input Error 1" & outcome()!="Input Error 2" & outcome()!="Please input your data"){
       BODYFAT=outcome()
-        if((BODYFAT<5 & input$Gender=="Men")        | (BODYFAT<13 & input$Gender=="Women")){
-          paste("According to the American Council on Exercise, your body fat falls into the essential body fat range, essential fat is the minimum amount of fat for your daily physical health, it is unhealthy to stay here")
-        }else if((BODYFAT<13 & input$Gender=="Men") | (BODYFAT<20 & input$Gender=="Women")){
-          paste("According to the American Council on Exercise, your body fat is in athletes range, please keep exercise and healthy diet")
-        }else if((BODYFAT<17 & input$Gender=="Men") | (BODYFAT<24 & input$Gender=="Women")){
-          paste("According to the American Council on Exercise, your body fat is in fitness range, please keep more exercise and healthier diet")
-        }else if((BODYFAT<24 & input$Gender=="Men") | (BODYFAT<31 & input$Gender=="Women")){
-          paste("According to the American Council on Exercise, your body fat is in acceptable range, you can pay attention to your physical exercise and avoid junk food")
+        if((BODYFAT<=5 & input$Gender=="Men")        | (BODYFAT<=13 & input$Gender=="Women")){
+          paste("According to the American Council on Exercise, your body fat percentage falls into the essential body fat range, essential fat is the minimum amount of fat for your daily physical health, it is unhealthy to stay here")
+        }else if((BODYFAT<=13 & input$Gender=="Men") | (BODYFAT<=20 & input$Gender=="Women")){
+          paste("According to the American Council on Exercise, your body fat percentage  is in athletes range, please keep exercise and healthy diet")
+        }else if((BODYFAT<=17 & input$Gender=="Men") | (BODYFAT<=24 & input$Gender=="Women")){
+          paste("According to the American Council on Exercise, your body fat percentage  is in fitness range, please keep more exercise and healthier diet")
+        }else if((BODYFAT<=24 & input$Gender=="Men") | (BODYFAT<=31 & input$Gender=="Women")){
+          paste("According to the American Council on Exercise, your body fat percentage  is in acceptable range, you can pay attention to your physical exercise and avoid junk food")
         }else{
-          paste("According to the American Council on Exercise, your body fat is in obesity range, please keep away from junk food, exercise everyday and monitor your own weight. If necessary, you should ask doctors for more advice")
+          paste("According to the American Council on Exercise, your body fat percentage  is in obesity range, please keep away from junk food, exercise everyday and monitor your own weight. If necessary, you should ask doctors for more advice")
         }
 
         
@@ -167,7 +167,9 @@ server<-shinyServer(function(input, output) {
           <br>
           ENZE WANG ewang36@wisc.edu<br> 
           <br>
-          RUI HUANG rhuang95@wisc.edu')
+          RUI HUANG rhuang95@wisc.edu<br>
+          <br>
+         ')
   })
   
   output$Introduction <- renderUI({
@@ -175,12 +177,15 @@ server<-shinyServer(function(input, output) {
           <br>
           This application is for Bodyfat subjects<br>
           <br>
-          This subject want to predict Bodyfat with your body data<br>
+          This subject want to predict body fat percentage with your body data<br>
+          <br>
+          Our rule is: BODYFAT(%) = 0.90*Abdomen circumference(cm) - 0.12*Weight(lbs) - 42.27<br>
           <br>
           Our application needs your weight and abdomen circumference, you can measure in English or metric units <br>
           <br>
           Please refer to Official Navy Abdominal Circumference Measurement Demonstration on youtube and other information on website if you want to know how to measure precise abdomen circumference. https://www.youtube.com/watch?v=ZH1aHB67bUk&ab_channel=personnelguru
-          ')
+          <br> 
+         ')
   })
   
   
@@ -189,15 +194,15 @@ server<-shinyServer(function(input, output) {
           <br>
           More information can be found in our group github: https://github.com/moslandwez/Module_2 <br>
           <br>
-          The bodyfat suggestion is given by the American Council on Exercise website: https://www.acefitness.org/<br>
+          The body fat suggestion is given by the American Council on Exercise website: https://www.acefitness.org/<br>
           <br>
-          This code and application is maintained by ENZE WANG<br>
+          This app code and application is maintained by ENZE WANG<br>
           <br> 
-          This application and the code structure is main edited by RUI HUANG <br> 
+          This application and the code structure is main edited by RUI HUANG and HanGyu KANG <br> 
           <br>
           The interface, ggplot and unit part are fixed, edited by ENZE WANG <br> 
           <br>
-          Other problems are fixed by HanGyu KANG and CHENYANG JIANG<br>
+          Other problems are fixed by  CHENYANG JIANG<br>
           <br>
           <br>
          ')
